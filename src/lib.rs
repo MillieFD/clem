ab6704f8ddc6adf4bc5417b411c3299d735a247a
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-04-15 10:49:41 +0100

    Add copyright header

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+/*
+Project: clem
+GitHub: https://github.com/MillieFD/clem
+
+BSD 3-Clause License, Copyright (c) 2026, Amelia Fraser-Dale
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the conditions of the LICENSE are met.
+*/
+
 //! Domain-agnostic high-throughput storage for n-dimensional analytical data.
 //!
 //! ---
