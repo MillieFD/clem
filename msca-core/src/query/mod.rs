0f051d267f974f594110d826e3c9ba99a489d8eb
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-08-07 07:09:24 +0100

    Refine `indexed` method signature in `query/mod.rs` to adjust lifetime and trait bounds

--- msca-core/src/query/mod.rs
+++ msca-core/src/query/mod.rs
@@ -97,11 +97,10 @@ impl<'m> Query<'m> {
     /// - [`Error::Io`] if a deserialization failure occurs.
     ///
     /// [1]: crate::dataset::Dataset
-    pub fn indexed<I, N>(&self) -> Result<HashMap<I, N, Xxh3Builder>, Error>
+    pub fn indexed<'q, I, N>(&'q self) -> Result<HashMap<I, N, Xxh3Builder>, Error>
     where
         N: Unsigned,
-        I: Read + Eq + Hash + 'static,
-        for<'q> I::Src<'q>: Composite<'q, Query<'q>> + Iterator<Item = Outcome<I>> + 'q,
+        I: Unfiltered<'q> + Eq + Hash + 'q,
     {
         let iter = self.iter::<I>()?;
         Self::intern(iter)
